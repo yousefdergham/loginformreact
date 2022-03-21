6c48d72c117e71eb3fa720f2b371d675765eca58
Yousef Dergham <79238462+yousefdergham@users.noreply.github.com>
2022-03-21 12:14:52 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Login from './components/login';
+import SignInOutContainer from './containers';
 function App() {
   return (
     <div className="App">
-      <Login />
+     <SignInOutContainer/>
     </div>
   );
 }
